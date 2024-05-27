d494bc93972769e081afa03a4b66d495fa86983e
josreimun <josreimun@alum.us.es>
2024-05-27 17:44:00 +0200

    Esquema página de inicio creado

--- backend/HDBAPI/settings.py
+++ backend/HDBAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +42,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'corsheaders',
-    'hermandabd'
+    'hermandabd',
+    'homepage'
 ]
 
 REST_FRAMEWORK = {
@@ -157,6 +159,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
