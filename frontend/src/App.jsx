023d9f9d7ed0121cc142678fa9c4ea67aaa45169
miguelybarra <miguel.ybarra.mym@gmail.com>
2024-06-04 21:23:01 +0200

    Añadido logout, activación de cuenta y registro, control estado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,29 +7,49 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import './index.css';
 import { LoginPage } from './pages/LoginPage';
+import { ActivateAccountPage } from './pages/ActivateAccountPage';
 import { MePage } from './pages/MePage';
 import { HermanoFormPage } from './pages/Hermano/HermanoFormPage';
 import { HermanosPage } from './pages/Hermano/HermanosPage';
 import PrivateRoute from './components/PrivateRoute';
+import { RegisterFormPage } from './pages/RegisterFormPage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/ReactToastify.min.css';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className='flex-grow'>
-          <Routes>
-            <Route path='/' element={<MainContent/>} /> {/* Por ahora se deja así pero en verdad se ponen en pages */}
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/me' element={<PrivateRoute element={MePage} />} /> 
-            <Route path='/hermano' element={<PrivateRoute element={HermanoFormPage} />} />
-            <Route path='/hermanos' element={<PrivateRoute element={HermanosPage} />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+  return <AuthProvider>
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={true}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            transition:Bounce />
+          <Header />
+          <main className='flex-grow'>
+            <Routes>
+              <Route path='/' element={<MainContent />} /> {/* Por ahora se deja así pero en verdad se ponen en pages */}
+              <Route path='/register' element={<RegisterFormPage />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path='/activate/:uid/:token' element={<ActivateAccountPage />} />
+              <Route path='/me' element={<PrivateRoute element={MePage} />} />
+              <Route path='/hermano' element={<PrivateRoute element={HermanoFormPage} />} />
+              <Route path='/hermanos' element={<PrivateRoute element={HermanosPage} />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
+  
 }
 
 export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { logout } from '../api/auth.api';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from "../context/AuthContext.jsx";
 
 function Header() {
+  const navigate = useNavigate();
+  const {logoutAction, isAuthenticated} = useContext(AuthContext);
+
+  const onLogout = () => {
+    logout();
+    logoutAction();
+    navigate('/');
+  }
+
+  const onLogin = () => {
+    navigate('/login');
+  }
+
   return (
     <header className="bg-red-600 text-white p-4 flex justify-between items-center">
       <div className="flex items-center">
         <span className="bg-red-800 text-white rounded-full px-2 py-1 mr-2">NUEVO</span>
         <span>COMENZANDO EL DESARROLLO INNOVADOR PARA LA GESTIÓN DE TU HERMANDAD!</span>
       </div>
-      
+      {
+        isAuthenticated ?
+          <button onClick={onLogout}>Logout</button> :
+          <button onClick={onLogin}>Login</button>
+      }
     </header>
   );
 }
