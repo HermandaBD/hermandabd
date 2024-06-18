7b8c145317d4ec4322137e971885245eff23f490
miguelybarra <miguel.ybarra.mym@gmail.com>
2024-06-18 17:47:02 +0200

    added netlify.toml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,17 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     manifest: true,
     outDir: 'dist',
     rollupOptions: {
-      input: '/src/main.jsx'
+      input: '/src/main.jsx',
+      output: {
+        manualChunks: undefined,
+      }
     }
-  },
-  server: {
-    port: 3000,
-  },
+  }
 });
 
 
